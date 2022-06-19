03c25a0233c6a7f6a3bd9dca8c13878cec890eea
7Romain <osiiriis@ymail.com>
2022-06-19 17:43:46 +0200

    ajout icone et choix inutiles cachés

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,8 @@
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.ImageIcon;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,9 +12,16 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import com.gluonhq.charm.glisten.control.Icon;
+import com.gluonhq.charm.glisten.control.ToggleButtonGroup;
 
 public class Controller implements Initializable {
 
@@ -21,6 +30,21 @@ public class Controller implements Initializable {
 
     @FXML
     private RadioButton ancienNon;
+
+    @FXML
+    private RadioButton rbtn0;
+
+    @FXML
+    private RadioButton rbtn1;
+
+    @FXML
+    private RadioButton rbtn2;
+
+    @FXML
+    private RadioButton rbtn3;
+
+    @FXML
+    private RadioButton rbtn4;
 
     @FXML
     private RadioButton ancienOui;
@@ -39,10 +63,69 @@ public class Controller implements Initializable {
     private String[] permis = { "moins de 2 ans", "2 ans et plus" };
 
     @FXML
-    private TextField nbAccident;
+    private ToggleGroup resp;
 
     @FXML
     private Label resultat;
+
+    @FXML
+    private ToggleButton tglRespNo;
+
+    @FXML
+    private ToggleButton tglRespYes;
+
+    @FXML
+    private ToggleButtonGroup tgbrs;
+
+    @FXML
+    private Label lblres;
+
+    @FXML
+    private Label lblnbacc;
+
+    @FXML
+    private HBox rBtnPn;
+
+    @FXML
+    private Label lblAnc;
+
+    @FXML
+    private HBox rBtnBAnc;
+
+    @FXML
+    void mouseclic(MouseEvent event) {
+        menuAnciennete();
+
+    }
+
+    private void menuAnciennete() {
+        if (choiceBox.getValue() != null) {
+            String permisAnc = choiceBox.getValue();
+            if (permisAnc.equals("2 ans et plus")) {
+                lblAnc.setVisible(true);
+                rBtnBAnc.setVisible(true);
+            }
+            if (permisAnc.equals("moins de 2 ans")) {
+                lblAnc.setVisible(false);
+                rBtnBAnc.setVisible(false);
+            }
+        }
+
+    }
+
+    @FXML
+    void montrerNbAcc(ActionEvent event) {
+        rBtnPn.setVisible(true);
+        lblnbacc.setVisible(true);
+
+    }
+
+    @FXML
+    void cacherNbAcc(ActionEvent event) {
+        rBtnPn.setVisible(false);
+        lblnbacc.setVisible(false);
+
+    }
 
     @FXML
     void calc(ActionEvent event) {
@@ -95,6 +178,33 @@ public class Controller implements Initializable {
         choiceBox.getItems().addAll(permis);
         choiceBox.setOnAction(this::getPermis);
 
+        Image imageyes = new Image("yesImage.png");
+        ImageView iv1 = new ImageView();
+        iv1.setImage(imageyes);
+        iv1.setFitWidth(27);
+        iv1.setPreserveRatio(true);
+        iv1.setSmooth(true);
+        iv1.setCache(true);
+        tglRespYes.setGraphic(iv1);
+
+        Image imageno = new Image("nonImage.png");
+        ImageView iv2 = new ImageView();
+        iv2.setImage(imageno);
+        iv2.setFitWidth(27);
+        iv2.setPreserveRatio(true);
+        iv2.setSmooth(true);
+        iv2.setCache(true);
+        tglRespNo.setGraphic(iv2);
+
+        rbtn0.setVisible(false);
+        rBtnPn.setVisible(false);
+        lblnbacc.setVisible(false);
+        lblAnc.setVisible(false);
+        rBtnBAnc.setVisible(false);
+
+        // choiceBox.getSelectionModel().selectedItemProperty().addListener(e ->
+        // menuAnciennete());
+
     }
 
     public boolean getPermis(ActionEvent e) {
@@ -126,7 +236,22 @@ public class Controller implements Initializable {
     }
 
     public int getNbAccident() {
-        return Integer.valueOf(nbAccident.getText());
+
+        if (tglRespNo.isSelected()) {
+            return 0;
+        }
+        if (tglRespYes.isSelected()) {
+            if (rbtn1.isSelected()) {
+                return 1;
+            }
+            if (rbtn2.isSelected()) {
+                return 2;
+            }
+            if (rbtn3.isSelected()) {
+                return 3;
+            }
+        }
+        return 4;
 
     }
 
